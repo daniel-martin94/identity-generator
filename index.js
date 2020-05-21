34ce891c02885bb638ec02edc882c0c26e1f7f8d
daniel-martin94 <danielpmartin94@gmail.com>
2020-05-21 16:27:15 -0700

    1 file modified

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function App() {
 
   const [gender, updateGender] = useState(null)
   const [age, updateAge] = useState(null)
-  const [location, updateLocation] = useState(null)
+  const [location, updateLocation] = useState("US")
 
   const [use, updateUser] = useState({})
 
@@ -30,26 +30,36 @@ function App() {
   function changeAge(min, max) {
     updateAge(Math.floor(Math.random() * (max - min) + min))
   }
-
   return (
     <div>
       <p>
         Let's generate a new identity
         </p>
       <div>
-        {gender == null && <h3> Select Gender </h3>}
-        {gender != null && <h3> Gender Selected: {gender} </h3>}
-        <button onClick={() => updateGender("Male")}>Male</button>
-        <button onClick={() => updateGender("Female")}>Female</button>
+        <label for="gender">Choose a gender: </label>
+          <select id="gender" value={gender} onChange={(e) => updateGender(e.target.value)}>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
       </div>
+      <br></br>
       <div>
-        {age == null && <h3> Select Age </h3>}
-        {age != null && <h3> Age Selected: {age} </h3>}
+        <label>Choose an age range: </label>
         <button onClick={(e) => changeAge(18, 25)}>18 to 25</button>
         <button onClick={(e) => changeAge(26, 35)}>26 to 35</button>
         <button onClick={(e) => changeAge(36, 55)}>36 to 55</button>
         <button onClick={(e) => changeAge(56, 65)}>56 to 65</button>
         <button onClick={(e) => changeAge(66, 115)}>66 to 115</button>
+      </div>
+      <br></br>
+      <div>
+        <label for="locations">Choose a location: </label>
+          <select id="locations" value={location} onChange={(e) => updateLocation(e.target.value)}>
+          <option value="AU">Australia (AU)</option>
+          <option value="NZ">New Zealand (NZ)</option>
+          <option value="GB">United Kingdom (GB)</option>
+          <option selected value="US">United States (US)</option>
+        </select>
       </div>
     </div>
   );
