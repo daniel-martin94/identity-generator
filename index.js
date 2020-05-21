7237e516ae2d5670ad3bc0aca8add05bdd680475
daniel-martin94 <danielpmartin94@gmail.com>
2020-05-21 15:41:14 -0700

    Added age and gender selections

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
+
+/*
+The purpose of this app is to gather basic information from the user
+(gender, age, and desired location)to generate a new id for them. 
+
+This app is mainly to learn the following:
+
+1. Using state to render a loading icon while 'fetching' data
+2. Using await async functions
+
+We should display the information asked, address, DOB, age, and email
+
+*/
+function App() {
+
+  const [gender, updateGender] = useState(null)
+  const [age, updateAge] = useState(null)
+  const [location, updateLocation] = useState(null)
+
+  const [use, updateUser] = useState({})
+
+  const [loading, setLoading] = useState(false)
+
+  function changeAge(min, max) {
+    updateAge(Math.floor(Math.random() * (max - min) + min))
   }
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
+  return (
+    <div>
+      <p>
+        Let's generate a new identity
         </p>
+      <div>
+        {gender == null && <h3> Select Gender </h3>}
+        {gender != null && <h3> Gender Selected: {gender} </h3>}
+        <button onClick={() => updateGender("Male")}>Male</button>
+        <button onClick={() => updateGender("Female")}>Female</button>
       </div>
-    );
-  }
+      <div>
+        {age == null && <h3> Select Age </h3>}
+        {age != null && <h3> Age Selected: {age} </h3>}
+        <button onClick={(e) => changeAge(18, 25)}>18 to 25</button>
+        <button onClick={(e) => changeAge(26, 35)}>26 to 35</button>
+        <button onClick={(e) => changeAge(36, 55)}>36 to 55</button>
+        <button onClick={(e) => changeAge(56, 65)}>56 to 65</button>
+        <button onClick={(e) => changeAge(66, 115)}>66 to 115</button>
+      </div>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById('root'));
