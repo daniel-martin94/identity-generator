e15f9d53bfcb1b3160dea76f0c30134674cea073
daniel-martin94 <danielpmartin94@gmail.com>
2020-05-25 12:37:01 -0700

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 import User from './User'
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+import { Dimmer, Loader, Image, Segment, Container } from 'semantic-ui-react'
 
 
 /*
@@ -34,13 +34,12 @@ function App() {
   }
 
   async function retrieveID() {
-    let api = 'https://randomuser.me/api/?gender='+ gender + '&nat=' + location + '&age=' + age
+    let api = 'https://randomuser.me/api/?gender=' + gender + '&nat=' + location + '&age=' + age
     setLoading(true);
-    let user = null
+    let tempUser = null
     await axios.get(api).then(response => {
-      console.log(response)
-      user = {
-        "name": response.data.results[0].name.first + " " +  response.data.results[0].name.last,
+      tempUser = {
+        "name": response.data.results[0].name.first + " " + response.data.results[0].name.last,
         "age": response.data.results[0].dob.age,
         "dob": response.data.results[0].dob.date,
         "phone": response.data.results[0].phone,
@@ -58,51 +57,57 @@ function App() {
     const delay = (ms) => new Promise(r => setTimeout(r, ms))
     await delay(3000)
     setLoading(false)
-    updateUser(user)
+    updateUser(tempUser)
   }
   return (
-    <div>
+    <Container>
       {loading == false && user == null && <div>
-      <p>
-        Let's generate a new identity
+        <p>
+          Let's generate a new identity
         </p>
-      <div>
-        <label for="gender">Choose a gender: </label>
+        <div>
+          <label for="gender">Choose a gender: </label>
           <select id="gender" value={gender} onChange={(e) => updateGender(e.target.value)}>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-      </div>
-      <br></br>
-      <div>
-        <label>Choose an age range: </label>
-        <button onClick={(e) => changeAge(18, 25)}>18 to 25</button>
-        <button onClick={(e) => changeAge(26, 35)}>26 to 35</button>
-        <button onClick={(e) => changeAge(36, 55)}>36 to 55</button>
-        <button onClick={(e) => changeAge(56, 65)}>56 to 65</button>
-        <button onClick={(e) => changeAge(66, 115)}>66 to 115</button>
-      </div>
-      <br></br>
-      <div>
-        <label for="locations">Choose a location: </label>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <br></br>
+        <div>
+          <label>Choose an age range: </label>
+          <button onClick={(e) => changeAge(18, 25)}>18 to 25</button>
+          <button onClick={(e) => changeAge(26, 35)}>26 to 35</button>
+          <button onClick={(e) => changeAge(36, 55)}>36 to 55</button>
+          <button onClick={(e) => changeAge(56, 65)}>56 to 65</button>
+          <button onClick={(e) => changeAge(66, 115)}>66 to 115</button>
+        </div>
+        <br></br>
+        <div>
+          <label for="locations">Choose a location: </label>
           <select id="locations" value={location} onChange={(e) => updateLocation(e.target.value)}>
-          <option value="AU">Australia (AU)</option>
-          <option value="NZ">New Zealand (NZ)</option>
-          <option value="GB">United Kingdom (GB)</option>
-          <option selected value="US">United States (US)</option>
-        </select>
-      </div>
-       <br></br>
-      {age && location && gender && 
-        <button onClick={() => retrieveID()}>Submit</button>
-      }
+            <option value="AU">Australia (AU)</option>
+            <option value="NZ">New Zealand (NZ)</option>
+            <option value="GB">United Kingdom (GB)</option>
+            <option selected value="US">United States (US)</option>
+          </select>
+        </div>
+        <br></br>
+        {age && location && gender &&
+          <button onClick={() => retrieveID()}>Submit</button>
+        }
       </div>}
 
-      {user != null && loading === false && 
+      {user === null && loading === true &&
+          <Dimmer active inverted>
+            <Loader inverted>Generating a new identity...</Loader>
+          </Dimmer>
+      }
+
+      {user != null && loading === false &&
         <User user={user}></User>
       }
 
-    </div>
+    </Container>
   );
 }
 
